dd63a3043a351ab2c44222c774fd931e61eafb19
DannEstulla <dannestulla@gmail.com>
2026-04-02 17:11:40 -0300

    chore: scaffold empty feature module Gradle configs

---
+++ feature/feed/presentation/build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+}
+
+kotlin {
+    androidTarget { compilerOptions { jvmTarget.set(JvmTarget.JVM_11) } }
+    iosX64(); iosArm64(); iosSimulatorArm64(); jvm()
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.core.domain)
+            implementation(projects.feature.feed.domain)
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.koin.core)
+        }
+        commonTest.dependencies { implementation(libs.kotlin.test) }
+    }
+}
+
+android {
+    namespace = "br.gohan.videofeed.feature.feed.presentation"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    compileOptions { sourceCompatibility = JavaVersion.VERSION_11; targetCompatibility = JavaVersion.VERSION_11 }
+    defaultConfig { minSdk = libs.versions.android.minSdk.get().toInt() }
+}
