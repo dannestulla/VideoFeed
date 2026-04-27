4733fe969efd31b74ec24c1bf0e8078da4e61d50
dannestulla <dannestulla@gmail.com>
2026-04-27 12:43:53 -0300

    fix: update Koin init call for Swift Export naming (no Kt prefix)

--- iosApp/iosApp/iosApp.swift
+++ iosApp/iosApp/iosApp.swift
@@ -4,7 +4,7 @@ import Shared
 @main
 struct iosAppApp: App {
     init() {
-        IOSKoinHelperKt.doInitKoin(baseUrl: "http://localhost:8081")
+        initKoin(baseUrl: "http://localhost:8081")
     }
 
     var body: some Scene {
